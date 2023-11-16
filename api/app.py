a53550a345886b8fe451cab3f00e5951f092f24d
Hunter Henrichsen <hunter.henrichsen@gmail.com>
2023-11-15 19:28:18 -0700

    Make the websockets talk to each other

--- api/app.py
+++ api/app.py
@@ -1,5 +1,5 @@
 import uvicorn
-from fastapi import FastAPI, WebSocket
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect
 from multiprocessing import Queue
 
 app = FastAPI()
@@ -8,12 +8,21 @@ app = FastAPI()
 @app.websocket("/ws")
 async def test(websocket: WebSocket):
     await websocket.accept()
-    while True:
-        text = app.queue.get()
-        print(f"[WEB]:\t Got {text}")
-        if text:
-            print(f"[WEB]:\t Sending: {text}")
-            await websocket.send_text(text)
+    print(f"[WEB]:\t Accepted Socket")
+    try:
+        while True:
+            text = ""
+            while not app.queue.empty():
+                part = app.queue.get()
+                text += " " + part
+                print(f"[WEB]:\t Got {part}")
+            if text:
+                print(f"[WEB]:\t Sending: {text}")
+                await websocket.send_text(text.strip())
+                await websocket.receive()
+    except WebSocketDisconnect:
+        print(f"[WEB]:\t Closed Socket")
+        await websocket.close()
 
 
 if __name__ == "__main__":
